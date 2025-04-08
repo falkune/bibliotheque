88bc0703f0d933b23396ccbaf6ad266096ee76d5
falkune <iboudiokel@gmail.com>
2025-04-09 01:33:42 +0200

    fghjkl

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit626b9e7ddd47fb7eff9aaa53cce0c9ad::getLoader();
+return ComposerAutoloaderInit1e3bff509a3a01352d1cfb45e9e4459c::getLoader();
